8134303426a4e4ff91c7d897950ab5f92241444a
JDMvDuB <97340577+JDMvDuB@users.noreply.github.com>
2024-05-30 10:53:43 +0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,4 @@
+<?php
+$link = mysqli_connect("localhost",'u2056747_smart_b','!D123i456m789a123!','u2056747_smart_button');
+// $link = mysqli_connect("localhost",'root','','smart_button');
+$link->query("SET NAMES utf8");
